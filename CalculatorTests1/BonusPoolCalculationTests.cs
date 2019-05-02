acfd82de0eb070e24ace4254cc5fae497fdfb552
Kinga Bernhardt <kbernhardt@synetec.co.uk>
2019-05-02 14:27:43 +0100

    (file)add test project

---
+++ CalculatorTests1/BonusPoolCalculationTests.cs
@@ -0,0 +1,175 @@
+﻿using InterviewTestTemplatev2.Controllers;
+using InterviewTestTemplatev2.Data;
+using InterviewTestTemplatev2.Models;
+using InterviewTestTemplatev2.Repositories;
+using InterviewTestTemplatev2.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+
+namespace CalculatorTests1
+{
+    [TestClass]
+    public class BonusPoolCalculationTests
+    {
+        [TestMethod]
+        public void CalculateBonusPercentageOfEmployeeSalary_Successful_When_It_Has_Correct_Input()
+        {
+            var employeeRepository = new Mock<IHrEmployeeRepository>();
+            var employee = new HrEmployee
+            {
+                ID = 3,
+                FistName = "John",
+                SecondName = "Smith",
+                DateOfBirth = Convert.ToDateTime("1985-09-05"),
+                HrDepartmentId = 1,
+                JobTitle = "Accountant",
+                Salary = 60000,
+                Full_Name = " John Smith"
+            };
+
+            List<HrEmployee> employees = new List<HrEmployee>()
+            {
+                new HrEmployee
+                {
+                    ID = 3,
+                    FistName = "John",
+                    SecondName = "Smith",
+                    DateOfBirth = Convert.ToDateTime("1985-09-05"),
+                    HrDepartmentId = 1,
+                    JobTitle = "Accountant",
+                    Salary = 60000,
+                    Full_Name = " John Smith"
+                },
+                new HrEmployee
+                {
+                    ID = 2,
+                    FistName = "Janet",
+                    SecondName = "Jones",
+                    DateOfBirth = Convert.ToDateTime("1975-09-05"),
+                    HrDepartmentId = 2,
+                    JobTitle = "HR Director",
+                    Salary = 90000,
+                    Full_Name = "Janet Jones"
+                }
+            };
+
+            employeeRepository.Setup(x => x.GetById(employee.ID))
+                .Returns(employee);
+            employeeRepository.Setup(x => x.Read())
+                .Returns(employees);
+
+            var service = new BonusPoolCalculationService(employeeRepository.Object);
+            var actual = service.CalculateBonusPercentageOfEmployeeSalary(employee.ID);
+
+            Assert.AreEqual(0.4m, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void CalculateBonusPercentageOfEmployeeSalary_When_TotalSalary_Is_Null_Throws_Exception()
+        {
+            var employeeRepository = new Mock<IHrEmployeeRepository>();
+            var employee = new HrEmployee
+            {
+                ID = 3,
+                FistName = "John",
+                SecondName = "Smith",
+                DateOfBirth = Convert.ToDateTime("1985-09-05"),
+                HrDepartmentId = 1,
+                JobTitle = "Accountant",
+                Salary = 35000,
+                Full_Name = " John Smith"
+            };
+
+            List<HrEmployee> employees = new List<HrEmployee>()
+            {
+                new HrEmployee
+                {
+                    ID = 3,
+                    FistName = "John",
+                    SecondName = "Smith",
+                    DateOfBirth = Convert.ToDateTime("1985-09-05"),
+                    HrDepartmentId = 1,
+                    JobTitle = "Accountant",
+                    Salary = 0,
+                    Full_Name = " John Smith"
+                },
+                new HrEmployee
+                {
+                    ID = 2,
+                    FistName = "Janet",
+                    SecondName = "Jones",
+                    DateOfBirth = Convert.ToDateTime("1975-09-05"),
+                    HrDepartmentId = 2,
+                    JobTitle = "HR Director",
+                    Salary = 0,
+                    Full_Name = "Janet Jones"
+                }
+            };
+
+            employeeRepository.Setup(x => x.GetById(employee.ID))
+                .Returns(employee);
+            employeeRepository.Setup(x => x.Read())
+                .Returns(employees);
+
+            var service = new BonusPoolCalculationService(employeeRepository.Object);
+            service.CalculateBonusPercentageOfEmployeeSalary(employee.ID);
+        }
+
+        [TestMethod]
+        public void CalculateBonusPoolForEmployee_Successful_When_It_Has_Correct_Input()
+        {
+            var bonusPoolAmount = 50;
+
+            var employee = new HrEmployee
+            {
+                ID = 3,
+                FistName = "John",
+                SecondName = "Smith",
+                DateOfBirth = Convert.ToDateTime("1985-09-05"),
+                HrDepartmentId = 1,
+                JobTitle = "Accountant",
+                Salary = 60000,
+                Full_Name = " John Smith"
+            };
+
+            List<HrEmployee> employees = new List<HrEmployee>()
+            {
+                new HrEmployee
+                {
+                    ID = 3,
+                    FistName = "John",
+                    SecondName = "Smith",
+                    DateOfBirth = Convert.ToDateTime("1985-09-05"),
+                    HrDepartmentId = 1,
+                    JobTitle = "Accountant",
+                    Salary = 60000,
+                    Full_Name = " John Smith"
+                },
+                new HrEmployee
+                {
+                    ID = 2,
+                    FistName = "Janet",
+                    SecondName = "Jones",
+                    DateOfBirth = Convert.ToDateTime("1975-09-05"),
+                    HrDepartmentId = 2,
+                    JobTitle = "HR Director",
+                    Salary = 90000,
+                    Full_Name = "Janet Jones"
+                }
+            };
+
+            var employeeRepository = new Mock<IHrEmployeeRepository>();
+            employeeRepository.Setup(x => x.GetById(employee.ID))
+                .Returns(employee);
+            employeeRepository.Setup(x => x.Read())
+                .Returns(employees);
+
+            var service = new BonusPoolCalculationService(employeeRepository.Object);
+            var actual = service.CalculateBonusPoolForEmployee(employee.ID, bonusPoolAmount);
+            Assert.AreEqual(20, actual);
+        }
+    }
+}
---
+++ CalculatorTests1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTests1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTests1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("69f5033c-2963-4c06-853a-264a31e3bc5d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
